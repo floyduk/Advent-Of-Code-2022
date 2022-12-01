1bf493d9fa78fb7dea6ae0f066530800091788d6
John Arnold <john@otn.co.uk>
2022-12-01 13:03:24 +0000

    Day 1 complete

---
+++ day1/d1p1.py
@@ -0,0 +1,23 @@
+import fileinput
+
+# elves_calories is an array of the total calories carried by each elf
+elves_calories = []
+
+# The current running total of calories carried by an elf
+this_elf_calories = 0
+
+# Iterate the list of calories adding numbers to this_elf_calories until we find a blank line
+for line in fileinput.input(files="day1/input.txt"):
+    line = line.rstrip()
+    if line == "":
+        # When we find a blank line we add the current running total to elves_calories array and start a new running total
+        elves_calories.append(this_elf_calories)
+        this_elf_calories = 0
+    else:
+        this_elf_calories += int(line)
+
+# When we exit the loop we've still not added the last elf's total to the list. So do that now.
+elves_calories.append(this_elf_calories)
+
+# Print the largest element in the elves_calories list
+print(max(elves_calories))

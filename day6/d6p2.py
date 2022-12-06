1a3e6a5f0b95c6f147012dd26688ec8b09b82782
John Arnold <floyduk@users.noreply.github.com>
2022-12-06 10:58:47 +0000

    Day 6 complete

---
+++ day6/d6p2.py
@@ -0,0 +1,11 @@
+# open and read the input file
+input_file = open("day6/input.txt", "r")
+stream = input_file.readline().rstrip()
+
+# Iterate through the string taking list slices of the string and comparing the length of that list 
+# with the length of the same list cast into a set (which will remove duplicates). If the lengths
+# are the same then the list contains no duplicates and we can print the position and end.
+for i in range(14, len(stream)):
+    if(len(stream[i-14:i]) == len(set(stream[i-14:i]))):
+        print(i)
+        break

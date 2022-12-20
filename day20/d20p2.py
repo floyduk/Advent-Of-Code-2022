1a1427fb4d0004725c2dd5353e0ec3480b753d69
John Arnold <floyduk@users.noreply.github.com>
2022-12-20 21:09:07 +0000

    Day 20 complete.

--- day20/d20p2.py
+++ day20/d20p2.py
@@ -1,6 +1,7 @@
 # open and read the input file
-input_file = open("day20/sample.txt", "r")
-input = [int(l) for l in input_file.read().split("\n")]
+input_file = open("day20/input.txt", "r")
+decryption_key = 811589153
+input = [int(l)*decryption_key for l in input_file.read().split("\n")]
 l = len(input)
 
 class node:
@@ -43,13 +44,19 @@ class node:
 
     def search_forward(self, count):
         n = self
-        for i in range(count % (l-1)):
+        for i in range(count % (l-1)):  # l-1 because we remove the node before searching
+            n = n.next
+        return n
+
+    def search_forward1000(self):
+        n = self
+        for i in range(1000 % l):
             n = n.next
         return n
 
     def search_backward(self, count):
         n = self
-        for i in range(count % (l-1)):
+        for i in range(count % (l-1)):  # l-1 because we remove the node before searching
             n = n.prev
         return n
 
@@ -70,24 +77,26 @@ node_order[0].prev = this_node
 # MAIN LOOP #
 #############
 
-for n in node_order:
-    if n.val == 0:
-        pass
-    elif n.val > 0:
-        old_loc = n.prev
-        n.remove()
-        n.insert(old_loc.search_forward(n.val))
-    else:
-        old_loc = n.prev
-        n.remove()
-        n.insert(old_loc.search_backward(abs(n.val)))
+for i in range(10):
+    for n in node_order:
+        if n.val == 0:
+            pass
+        elif n.val > 0:
+            old_loc = n.prev
+            n.remove()
+            n.insert(old_loc.search_forward(n.val))
+        else:
+            old_loc = n.prev
+            n.remove()
+            n.insert(old_loc.search_backward(abs(n.val)))
 
-    node_order[0].print_list()
+    # node_order[0].print_list()
+
 
 zero_node = node_order[0].find(0)
-val1_node = zero_node.search_forward(1000)
-val2_node = val1_node.search_forward(1000)
-val3_node = val2_node.search_forward(1000)
+val1_node = zero_node.search_forward1000()
+val2_node = val1_node.search_forward1000()
+val3_node = val2_node.search_forward1000()
 
 print(f"{val1_node.val} + {val2_node.val} + {val3_node.val} = ", end="")
 print(val1_node.val + val2_node.val + val3_node.val)
